2e2efdc19fe192b82b5c137e8c583c1003f2d433
Kabir192003 <160172047+Kabir192003@users.noreply.github.com>
2026-03-03 21:15:16 +0000

    Update Project1.jsx

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -19,7 +19,7 @@ const Project1 = () => {
         persona1: './tmp11.jpg',
         persona2: './tmp22.jpg',
         sitemap: './tmsm.jpg',
-        wireframes: ['./tmw1.jpg', './tmw2.jpg', './tmw3.jpg'],
+        wireframes: ['./tmw11.jpg', './tmw22.jpg', './tmw33.jpg'],
         finalMockups: ['./tmf1.jpg', './tmf222.jpg', './tmf333.jpg']
     };
 
